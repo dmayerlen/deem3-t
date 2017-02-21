eed7467c5c1b9080eb85982696b7a1544726b914
David Mayerlen <david@deem3.com>
2017-02-20 19:53:47 -0500

    A file for testing xdebug configuration

---
+++ td.php
@@ -0,0 +1,31 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class test {
+    public $pub = false;
+    private $priv = true;
+    protected $prot = 42;
+}
+$t = new test;
+$t->pub = $t;
+$data = array(
+    'one' => 'a somewhat long string!',
+    'two' => array(
+        'two.one' => array(
+            'two.one.zero' => 210,
+            'two.one.one' => array(
+                'two.one.one.zero' => 3.141592564,
+                'two.one.one.one'  => 2.7,
+            ),
+        ),
+    ),
+    'three' => $t,
+    'four' => range(0, 5),
+);
+var_dump( $data );
+
